9a7c29f9d29888fc965b5f51b6953e6b4c803473
Ajay2307 <ajay.prodevans@gmail.com>
2019-08-05 11:42:47 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,37 @@
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pickle
+import pandas as pd
+infile = open("files/senti_prediction_NB.pickle",'rb')
+data = pickle.load(infile)
+infile.close()
+#print('im')
+x=data[data==2]
+
+#print(x)
+y=data[data==1]
+z=data[data==0]
+
+app = dash.Dash()
+
+app.layout = html.Div(children=[
+    html.H1(children='Welcome to prodevanss'),
+    dcc.Graph(id='example',
+        figure={
+            'data': [
+                {'x': [0,1,2],'y ':[y],'type': 'bar', 'name': 'Negative sentiment'},
+                {'x': [0,1,2],'y ':[z],'type': 'bar', 'name': 'Positive sentiment'},
+                
+                {'x': [0,1,2],'y ':[x],'type': 'bar', 'name': 'Neutral sentiment'},
+            ],
+            'layout': {
+                'title': 'Sentiment Analysis for drugs/medicines'
+            }
+        }
+            )
+])
+
+if __name__ == "__main__":
+    app.run_server(debug=True,host='0.0.0.0')
