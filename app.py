777b32722acba79bfcb6397b5df84e5aaeaa5ce4
Ajay2307 <ajay.prodevans@gmail.com>
2019-08-10 08:49:44 +0000

    changed title

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ z=data[data==0]
 app = dash.Dash()
 
 app.layout = html.Div(children=[
-    html.H1(children='Welcome to Prodevans Tech'),
+    html.H1(children='Hello Bangalore'),
     dcc.Graph(id='example',
         figure={
             'data': [
